d0f0b19ab42a52bf5aaa490098a9cbf31014e9c9
Isaac Pinheiro <isaacpnhr@gmail.com>
2022-03-08 00:16:14 -0300

    Adicionando as libs.

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# TODO
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index(path):
+    return 'Simple Auth Proxy: ' + path
+
+if __name__ == '__main__':
+    app.run()
 
