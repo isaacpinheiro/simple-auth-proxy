3a9b6026853b3c9b9e90ded91090587ddb40f600
Isaac Pinheiro <isaacpnhr@gmail.com>
2022-03-08 15:16:21 -0300

    Implementando o método get_params_string().

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
+def get_params_string(params):
+
+    res = ''
+    keys = list(params.keys())
+
+    for k in keys:
+
+        if res == '':
+            res += '?' + k + '=' + str(params[k])
+        else:
+            res += '&' + k + '=' + str(params[k])
+
+    return res
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def index(path):
-    return 'Simple Auth Proxy: ' + path
+    return 'Simple Auth Proxy: ' + path + get_params_string(request.args)
 
 if __name__ == '__main__':
     app.run()
